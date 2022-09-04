bae17505f95359425c44cf12621c8c15c25d1a72
TomasAisti <tomasaristimuno@gmail.com>
2022-09-04 19:35:13 -0300

    C:/Program Files/Git/

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -1,9 +1,8 @@
 module.exports = {
     admin: (req, res) => {
-        
-        if(req.session.loggedUser.email == "dimaria@gmail.com") {
+        if (req.session.loggedUser.email == "dimaria@gmail.com") {
             res.render("./product/adminpanel")
-            console.log("iupi");
+            console.log("iupi")
         }
         console.log("no tenes permiso")
     },
