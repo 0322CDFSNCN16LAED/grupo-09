e472c5c6c8d0678e02c6c47e5caf2b999186f419
geracons <martin13rc@gmail.com>
2022-09-11 12:09:41 -0300

    fix dash

--- dashboard/src/components/tours/UltimoTour.jsx
+++ dashboard/src/components/tours/UltimoTour.jsx
@@ -36,7 +36,7 @@ export default function UltimoTour() {
                     className="img-fluid px-3 px-sm-4 mt-3 mb-4"
                     style={{ width: "40rem" }}
                     src={`http://localhost:3001/images/productsimages/${producto.image}`}
-                    alt=" Star Wars - Mandalorian "
+                    alt={`${producto.name}`}
                 />
             </div>
             <p>
