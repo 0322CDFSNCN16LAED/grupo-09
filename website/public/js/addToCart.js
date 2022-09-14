4c81b91c7ce573452c3989775d0e0414da618e84
TomasAisti <tomasaristimuno@gmail.com>
2022-09-14 15:52:24 -0300

    agredado cartel confirmacion de compra

--- website/public/js/addToCart.js
+++ website/public/js/addToCart.js
@@ -1,14 +1,7 @@
 let buttonAddTToCart = document.querySelector(".addButton")
 let addedToCart = document.querySelector("#addedToCart")
 let deleteItem = document.querySelector("#deleteItem")
-let selectPeople = document.querySelector("#selectPeople")
-let price = document.querySelector("#price")
-let pricePerPeople = document.querySelector("#pricePerPeople")
-let select = document.getElementById("selectPeople")
-let selectValue = select.value
-
-const opinion =
-    " this has been one of yhe best tours I have ever made. The guidance was great and the path we follow arrown the city was marvlouswefwewqf fef4  33r5asc r3rdf"
+let confirmItem = document.querySelector("#confirmItem")
 
 addedToCart.classList.add("hide")
 window.onload = function () {
@@ -18,7 +11,7 @@ window.onload = function () {
     deleteItem.addEventListener("click", () => {
         addedToCart.classList.add("hide")
     })
-    if ((selectValue = 2)) {
-        price.innerHTML = price + 2
-    }
+    confirmItem.addEventListener("click", () => {
+        swal("Excelente", "Gracias por elegirnos!", "success")
+    })
 }
