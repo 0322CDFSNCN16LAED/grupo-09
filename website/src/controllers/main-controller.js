35eda3394697b22bb0be465271b2e035e76197c6
TomasAisti <tomasaristimuno@gmail.com>
2022-09-17 19:42:32 -0300

    cards de productos arregladas

--- website/src/controllers/main-controller.js
+++ website/src/controllers/main-controller.js
@@ -1,14 +1,15 @@
 module.exports = {
     admin: (req, res) => {
-        if(!req.session.loggedUser){
+        if (!req.session.loggedUser) {
             res.redirect("/")
-        }
-        else if (req.session.loggedUser.email == "admin@tripstarter.com") {
+        } else if (
+            req.session.loggedUser.email == "admin@tripstarter.com" ||
+            "tara@gmail.com"
+        ) {
             res.render("./product/adminpanel")
             console.log("iupi")
         } else {
             res.redirect("/")
         }
- 
     },
 }
